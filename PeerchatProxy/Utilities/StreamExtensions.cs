d95e1c3969e62838acf8b085e4c8ce09b4334eff
Matt Stevens <matt@handsomematt.co.uk>
2016-10-26 23:08:53 +0100

    async extensions

---
+++ PeerchatProxy/Utilities/StreamExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PeerchatProxy
+{
+    internal static class StreamExtensions
+    {
+        public static async Task<string> ReadLineAsync(this StreamReader sr, CancellationToken cancellationToken)
+        {
+            return await sr.ReadLineAsync().WithCancellation(cancellationToken);
+        }
+
+        public static async Task WriteLineAsync(this StreamWriter sw, string data, CancellationToken cancellationToken)
+        {
+            await sw.WriteLineAsync(data).WithCancellation(cancellationToken);
+        }
+    }
+}
